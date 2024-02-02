e7762ddc9fa4d9fb56a895bf0a3d366a938c7488
sai shravan <saishravan384@gmail.com>
2024-02-02 21:37:25 +0530

    feat: Add Logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import '../styles/Logo.css'
+
+export default function Logo() {
+  return (
+    <div className='logo'>
+      <img
+        src='/marvel_logo.png'
+        alt='the marvel logo with white text against a red background'
+      />
+      <h1>Memory Game</h1>
+    </div>
+  )
+}
