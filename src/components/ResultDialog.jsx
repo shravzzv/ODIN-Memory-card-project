892a8eb989df37d1a5bf4510a0eb6645c07ae4eb
sai shravan <saishravan384@gmail.com>
2024-02-03 21:27:13 +0530

    styles: Use comic bg for result dialog

--- src/components/ResultDialog.jsx
+++ src/components/ResultDialog.jsx
@@ -2,9 +2,11 @@ import './../styles/ResultDialog.css'
 
 export default function ResultDialog({ isWin, showDialog, handleRestart }) {
   return (
-    <dialog open={showDialog}>
-      {isWin ? <h2>Congratulations! You won.</h2> : <h2>You Lost!</h2>}
-      <button onClick={handleRestart}>Restart</button>
+    <dialog open={showDialog} className={isWin ? 'win' : 'loss'}>
+      <div className='content'>
+        {isWin ? <h2>Congratulations! You won.</h2> : <h2>You Lost!</h2>}
+        <button onClick={handleRestart}>Restart</button>
+      </div>
     </dialog>
   )
 }
